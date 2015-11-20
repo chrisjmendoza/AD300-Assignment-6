65b346a3babddf4d76691f15eed5c72e0c796e4e
Chris Mendoza <chrisjmendoza@gmail.com>
2015-11-19 20:06:51 -0800

    Update HuffmanEncode.java

--- HuffmanEncode.java
+++ HuffmanEncode.java
@@ -1,9 +1,4 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.Reader;
+import java.io.*;
 import java.util.*;
 
 public class HuffmanEncode<T> implements Comparable<T> {
